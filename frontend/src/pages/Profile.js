87266b5c5706269f78ce70b13f92d205db3dc3fc
Tristan <tristanholup@gmail.com>
2023-03-22 00:04:28 -0500

    example of using auth context instead of other way

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,26 +2,30 @@ import React, { useState } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
-import { auth } from "../../firebase.config";
+// import { auth } from "../../firebase.config";
+import { useAuth } from "../contexts/AuthContext";
 
 import "./Profile.css";
 
 import TestImg from "../assets/test.png";
 
 const Profile = () => {
-    const [firstName, setFirstName] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("XXX-XXX-XXXX");
+    // const [firstName, setFirstName] = useState("");
+    // const [phoneNumber, setPhoneNumber] = useState("XXX-XXX-XXXX");
 
     const navigate = useNavigate();
+    const { currentUser } = useAuth()
 
-    onAuthStateChanged(auth, (currentUser) => {
-        if (currentUser) {
-            setFirstName(currentUser.uid);
-            setPhoneNumber(currentUser.phoneNumber);
-        } else {
-            console.log("User is not logged in.");
-        }
-    });
+    // onAuthStateChanged(auth, (currentUser) => {
+    //     if (currentUser) {
+    //         setFirstName(currentUser.uid);
+    //         setPhoneNumber(currentUser.phoneNumber);
+    //     } else {
+    //         console.log("User is not logged in.");
+    //     }
+    // });
+
+    
 
     const handleEditProfile = () => {
         alert("TODO: Editing profile...");
@@ -43,9 +47,9 @@ const Profile = () => {
             <div className="header">
                 <img className="user-profile" src={TestImg} alt="User" />
                 <div className="user-info">
-                    <h1>Hey, {firstName}!</h1>
+                    <h1>Hey, {currentUser.uid}!</h1>
                     <p>
-                        <strong>Phone Number:</strong> {phoneNumber}
+                        <strong>Phone Number:</strong> {currentUser.phoneNumber}
                     </p>
                     <button onClick={handleEditProfile}>Edit Profile</button>
                     <button onClick={handleLogout}>Logout</button>
